123587c1005231014fab889a24ab4d105aa81e0b
rohan-darji <rohandarji2001@gmail.com>
2024-06-19 00:38:01 -0400

    database files setup

---
+++ app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,22 @@
+package com.example.cleanarchitecturenoteapp.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.cleanarchitecturenoteapp.ui.theme.BabyBlue
+import com.example.cleanarchitecturenoteapp.ui.theme.LightGreen
+import com.example.cleanarchitecturenoteapp.ui.theme.RedOrange
+import com.example.cleanarchitecturenoteapp.ui.theme.RedPink
+import com.example.cleanarchitecturenoteapp.ui.theme.Violet
+
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int = 0
+) {
+    companion object{
+        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    }
+}
---
+++ app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/domain/util/OrderType.kt
@@ -0,0 +1,7 @@
+package com.example.cleanarchitecturenoteapp.feature_note.domain.util
+
+sealed class OrderType {
+    object Ascending : OrderType()
+    object Descending : OrderType()
+
+}
