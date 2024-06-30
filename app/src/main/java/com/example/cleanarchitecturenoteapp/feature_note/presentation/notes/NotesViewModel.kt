b383f2094effd7fe4c39b86443f990db8b97ed9f
rohan-darji <rohandarji2001@gmail.com>
2024-06-30 18:34:24 -0400

    Fixed the sort animation ont working

--- app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.cleanarchitecturenoteapp.feature_note.presentation.notes
 
+import androidx.compose.runtime.Recomposer
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.cleanarchitecturenoteapp.feature_note.domain.model.Note
@@ -22,8 +25,8 @@ class NotesViewModel @Inject constructor(
     private val noteUseCases: NoteUseCases
 ) : ViewModel() {
 
-    private val _state = MutableStateFlow(NotesState())
-    val state: StateFlow<NotesState> = _state
+    private val _state = mutableStateOf(NotesState())
+    val state: State<NotesState> = _state
 
     private var recentlyDeletedNote: Note? = null
 
@@ -36,7 +39,9 @@ class NotesViewModel @Inject constructor(
     fun onEvent(event: NotesEvent) {
         when (event) {
             is NotesEvent.Order -> {
-                if (state.value.noteOrder::class == event.noteOrder::class && state.value.noteOrder.orderType == event.noteOrder.orderType) {
+                if (state.value.noteOrder::class == event.noteOrder::class &&
+                    state.value.noteOrder.orderType == event.noteOrder.orderType
+                    ) {
                     return
                 }
                 getNotes(event.noteOrder)
